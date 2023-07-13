bba20bc263258985f39eed6a6c528f73fd030d3c
Albertito <fuentescarlosalberto98@gmail.com>
2023-07-13 17:55:07 -0500

    Guiones resueltos

--- src/Components/Notas/CrearGuion.jsx
+++ src/Components/Notas/CrearGuion.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 
 import {FaAngleLeft} from 'react-icons/fa';
 import { FaPlusSquare } from "react-icons/fa";
@@ -6,142 +5,134 @@ import { FaSave } from 'react-icons/fa';
 import { FaMinusSquare } from 'react-icons/fa'
 
 
+import React, { useState, useEffect } from 'react';
 
+const NuevoGuion = () => {
+  const [textAreas, setTextAreas] = useState(['Texto largo...']);
 
-class NuevoGuion extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        textAreas: ["Texto largo..."], // Array inicial con un textarea
-      };
-    }
-  
-    handleAddTextArea = () => {
-      this.setState((prevState) => ({
-        textAreas: [...prevState.textAreas, ""], // Agregar un textarea vacío al array
-      }));
-    };
-  
-    handleRemoveTextArea = (index) => {
-      this.setState((prevState) => {
-        const updatedTextAreas = [...prevState.textAreas];
-        updatedTextAreas.splice(index, 1); // Eliminar el textarea en el índice especificado
-        return { textAreas: updatedTextAreas };
-      });
-    };
-  
-    handleChange = (event, index) => {
-      const { value } = event.target;
-      this.setState((prevState) => {
-        const updatedTextAreas = [...prevState.textAreas];
-        updatedTextAreas[index] = value; // Actualizar el valor del textarea en el índice correspondiente
-        return { textAreas: updatedTextAreas };
-      });
-    };
+  const handleAddTextArea = () => {
+    setTextAreas((prevState) => [...prevState, '']);
+  };
 
-    handleInput = (event, index) => {
-      const { value, scrollHeight } = event.target;
-      event.target.style.height = scrollHeight + "px";
-  
-      this.setState((prevState) => {
-        const updatedTextAreas = [...prevState.textAreas];
-        updatedTextAreas[index] = value;
-        return { textAreas: updatedTextAreas };
-      });
-    };
+  const [textareaValue, setTextareaValue] = useState('');
 
-    addEventListener() {
-      const textareaLeft = document.getElementById('textarea-left');
-      const textareaRight = document.getElementById('textarea-right');
-    
-      textareaLeft.addEventListener('input', function() {
-        const inputText = textareaLeft.value;
-        textareaRight.value = inputText;
-        textareaRight.style.height = textareaLeft.scrollHeight + 'px';
-      });
-    
-      textareaLeft.addEventListener('scroll', function() {
-        textareaRight.scrollTop = textareaLeft.scrollTop;
-      });
+  const handleChangeHeight = (large) => {
+    const { value } = large.target;
+    setTextareaValue(value);
+    large.target.style.height = 'auto';
+    large.target.style.height = large.target.scrollHeight + 'px';
+  };
+
+  const handleRemoveTextArea = (index) => {
+    const updatedTextAreas = [...textAreas];
+    updatedTextAreas.splice(index, 1);
+    setTextAreas(updatedTextAreas);
+  };
+
+  const handleChange = (event, index) => {
+    const { value } = event.target;
+    const updatedTextAreas = [...textAreas];
+    updatedTextAreas[index] = value;
+    setTextAreas(updatedTextAreas);
+  };
+
+  const handleInput = (event, index) => {
+    const { value, scrollHeight } = event.target;
+    event.target.style.height = scrollHeight + 'px';
+    const updatedTextAreas = [...textAreas];
+    updatedTextAreas[index] = value;
+    setTextAreas(updatedTextAreas);
+  };
+
+  useEffect(() => {
+    const textareaLeft = document.getElementById('textarea-left');
+    const textareaRight = document.getElementById('textarea-right');
+
+    const handleTextareaInput = () => {
+      const inputText = textareaLeft.value;
+      textareaRight.value = inputText;
+      textareaRight.style.height = textareaLeft.scrollHeight + 'px';
     };
 
-    
+    const handleTextareaScroll = () => {
+      textareaRight.scrollTop = textareaLeft.scrollTop;
+    };
 
+    textareaLeft.addEventListener('input', handleTextareaInput);
+    textareaLeft.addEventListener('scroll', handleTextareaScroll);
 
-    render() {
+    return () => {
+      textareaLeft.removeEventListener('input', handleTextareaInput);
+      textareaLeft.removeEventListener('scroll', handleTextareaScroll);
+    };
+  }, []);
 
-      document.addEventListener('DOMContentLoaded', function() {
-        const textareaLeft = document.getElementById('textarea-left');
-        const textareaRight = document.getElementById('textarea-right');
-      
-        textareaLeft.addEventListener('input', function() {
-          const inputText = textareaLeft.value;
-          textareaRight.value = inputText;
-          textareaRight.style.height = textareaLeft.scrollHeight + 'px';
-        });
-      
-        textareaLeft.addEventListener('scroll', function() {
-          textareaRight.scrollTop = textareaLeft.scrollTop;
-        });
-      });
-      
-
-      return (
-                <div className="Auth-form-container">
-        <form className="Auth-form-Guion">
+  return (
+    <div className="Auth-form-container">
+      <form className="Auth-form-Guion">
         <div className="Auth-form-content">
-            <h2 className="Auth-form-title">Crear guión</h2>
-            <h3 className="Text-helper">Escriba en las cuadrillas a continuación los guiones que desea agregar a la nota </h3>
-            <div>
-                <h3>TITULO: SAMPLE TEXT </h3>
+          <h2 className="Auth-form-title">Crear guión</h2>
+          <h3 className="Text-helper">Escriba en las cuadrillas a continuación los guiones que desea agregar a la nota</h3>
+          <div>
+            <h3>TITULO: SAMPLE TEXT</h3>
+          </div>
+          <br />
+          <div>
+            <form className="Button-form">
+              <button type="button" className="btn btn-dark">
+                <FaAngleLeft size={20} color="white" /> Regresar
+              </button>
+              <button type="button" className="btn btn-success">
+                <FaSave size={20} color="white" /> Guardar cambios
+              </button>
+              <button type="button" className="btn btn-primary" onClick={handleAddTextArea}>
+                <FaPlusSquare size={20} color="white" /> Agregar Celda
+              </button>
+              <button type="button" className="btn btn-danger" onClick={() => handleRemoveTextArea()}>
+                <FaMinusSquare size={20} color="white" /> Quitar Celda
+              </button>
+            </form>
+          </div>
+          <br />
+          <div>
+            {textAreas.map((text, index) => (
+              <div className="textarea-container" key={index}>
+                <textarea
+                  className="textarea-left"
+                  id="textarea-left"
+                  // value={text}
+                  placeholder='Notación'
+                  onChange={(event) => handleChange(event, index)}
+                  onInput={(event) => handleInput(event, index)}
+                  onChangeHeight={handleChange}
+                  rows={10}
+                  style={{ width: "300px", resize: "none", height: "80px" }}
+                />
 
-            </div>
-            <br />
-            <div>
-                <form className="Button-form">
-                    <button type="button" class="btn btn-dark"> <FaAngleLeft size={20} color="white"/> Regresar</button>
-                    <button type="button" class='btn btn-success'> <FaSave size={20} color="white"/> Guardar cambios </button>
-                    <button type="button" class="btn btn-primary"  onClick={this.handleAddTextArea}> <FaPlusSquare  size={20} color="white"/> Agregar Celda</button>
-                    <button type="button" class="btn btn-danger" onClick={() => this.handleRemoveTextArea()} > <FaMinusSquare  size={20} color="white"/> Quitar Celda</button>
+                <textarea
+                  className="textarea-right"
+                  id="textarea-right"
+                  // value={text}
+                  placeholder='Acotación'
 
-                </form>
-            </div>
-            <br />
-                <div>
-                  {this.state.textAreas.map((text, index) => (
-
-                  <div class="textarea-container" key={index}>
-                        <textarea class="textarea-left"
-                         id="textarea-left"
-                        type="text"
-                        onChange={(event) => this.handleChange(event, index)}
-                        onInput={(event) => this.handleInput(event, index)}
-                        rows={10}
-                        style={{ width: "100%", resize: "none", height: "80px" }}
-                        />
-
-                        <textarea class="textarea-right"
-                         id="textarea-right"
-                        type="text"
-                        onChange={(event) => this.handleChange(event, index)}
-                        onInput={(event) => this.handleInput(event, index)}
-                        rows={10}
-                        style={{ width: "100%", resize: "none", height: "80px"}}
-                        />
-                  </div>
-              ))}
-
-                </div>
+                  rows={10}
+                  style={{ width: "300px", resize: "none", height: "80px" }}
+                />
+              </div>
+            ))}
+          </div>
         </div>
-            <br />
-        
-        </form>
+        <br />
+      </form>
     </div>
-      );
-    }
-  }
-  
+  );
+};
 
-  export default NuevoGuion
+export default NuevoGuion;
 
 
+
+// onChange={(event) => {
+//   handleChange(event);
+//   this.handleChange(event, index);
+// }}
--- src/Components/Notas/PRUEBACELDA.jsx
+++ src/Components/Notas/PRUEBACELDA.jsx
@@ -1,99 +1,85 @@
-// import React from "react";
+// // import React from "react";
+// import React, { useState } from 'react';
 
 
-// class DynamicTextAreaComponent extends React.Component {
-//     constructor(props) {
-//       super(props);
-//       this.state = {
-//         textAreas: ["Texto largo..."], // Array inicial con un textarea
-//       };
-//     }
-  
-//     handleAddTextArea = () => {
-//       this.setState((prevState) => ({
-//         textAreas: [...prevState.textAreas, ""], // Agregar un textarea vacío al array
-//       }));
-//     };
-  
-//     handleRemoveTextArea = (index) => {
-//       this.setState((prevState) => {
-//         const updatedTextAreas = [...prevState.textAreas];
-//         updatedTextAreas.splice(index, 1); // Eliminar el textarea en el índice especificado
-//         return { textAreas: updatedTextAreas };
-//       });
-//     };
-  
-//     handleChange = (event, index) => {
-//       const { value } = event.target;
-//       this.setState((prevState) => {
-//         const updatedTextAreas = [...prevState.textAreas];
-//         updatedTextAreas[index] = value; // Actualizar el valor del textarea en el índice correspondiente
-//         return { textAreas: updatedTextAreas };
-//       });
-//     };
-  
-//     render() {
-//       return (
-//         <div>
-//           {this.state.textAreas.map((text, index) => (
-//             <div className="Row-Guion" key={index}>
-//               <textarea
-//                 type="text"
-//                 className="excel-cell-input"
-//                 // value={text}
-//                 onChange={(event) => this.handleChange(event, index)}
-//                 rows={10}
-//                 style={{ width: "100%", resize: "none" }}
-//               />
-//                <textarea
-//                 type="text"
-//                 className="excel-cell-input"
-//                 // value={text}
-//                 onChange={(event) => this.handleChange(event, index)}
-//                 rows={10}
-//                 style={{ width: "100%", resize: "none" }}
-//               />
-//               <button onClick={() => this.handleRemoveTextArea(index)}>
-//                 Eliminar
-//               </button>
-//             </div>
-//           ))}
-//           <button onClick={this.handleAddTextArea}>Agregar textarea</button>
-//         </div>
-//       );
-//     }
-//   }
-  
 
-//   export default DynamicTextAreaComponent
+// const MyComponent = () => {
+//   const [textareaValue, setTextareaValue] = useState('');
+
+//   const handleChange = (event) => {
+//     const { value } = event.target;
+//     setTextareaValue(value);
+//     event.target.style.height = 'auto';
+//     event.target.style.height = event.target.scrollHeight + 'px';
+//   };
+
+//   return (
+//     <div class="textarea-container" >
+//           <textarea
+//             class="textarea-left"
+//              id="textarea-left"
+//              style={{ width: "300px", resize: "none", height: "80px" }}
+//             onChange={handleChange}
+//           ></textarea>
+//           <textarea
+//             class="textarea-right"
+//              id="textarea-right"
+//              style={{ width: "300px", resize: "none"}}
+//           ></textarea>
+//     </div>
+
+//   );
+// };
+
+// export default MyComponent;
 
 
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const MyComponent = () => {
-  const [textareaValue, setTextareaValue] = useState('');
+  const [textAreas, setTextAreas] = useState(["Texto largo..."]);
 
-  const handleChange = (event) => {
+    const [textareaValue, setTextareaValue] = useState('');
+
+  const handleAddTextArea = () => {
+    setTextAreas((prevState) => [...prevState, ""]);
+  };
+
+  const handleRemoveTextArea = (index) => {
+    const updatedTextAreas = [...textAreas];
+    updatedTextAreas.splice(index, 1);
+    setTextAreas(updatedTextAreas);
+  };
+
+  const handleChange = (event, index) => {
     const { value } = event.target;
+    const updatedTextAreas = [...textAreas];
+    updatedTextAreas[index] = value;
+    setTextAreas(updatedTextAreas);
     setTextareaValue(value);
     event.target.style.height = 'auto';
     event.target.style.height = event.target.scrollHeight + 'px';
   };
 
   return (
-    <div className="Row-Guion">
+    <div>
+      {textAreas.map((text, index) => (
+            <div class="textarea-container" >
           <textarea
-            id="myTextarea"
-            value={textareaValue}
+            class="textarea-left"
+             id="textarea-left"
+             style={{ width: "300px", resize: "none", height: "80px" }}
             onChange={handleChange}
           ></textarea>
           <textarea
-            id="myTextarea"
-            value={textareaValue}
-            onChange={handleChange}
+            class="textarea-right"
+             id="textarea-right"
+             style={{ width: "300px", resize: "none"}}
           ></textarea>
+     </div>
+      ))}
+      <button onClick={handleAddTextArea}>Agregar textarea</button>
     </div>
-
   );
 };
 
