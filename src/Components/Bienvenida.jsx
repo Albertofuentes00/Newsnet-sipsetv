6a21d3d154e59fef1745500f317bba7668522a09
LeonardoDRebollo <leonardod.rebollo@gmail.com>
2023-09-17 00:57:36 -0500

    Login

--- src/Components/Bienvenida.jsx
+++ src/Components/Bienvenida.jsx
@@ -1,8 +1,26 @@
-const Texto = (props) => {
-    return (
-        <h3>Bienvenido: {props.name} {props.apellidos} </h3>
+import Cookies from "js-cookie";
 
-    )
+const Texto = (props) => {
+
+
+    const user = Cookies.get('Usuario');
+    const rutaActual = window.location.pathname;
+
+    // Realizar una acción basada en la ruta actual
+    if (rutaActual != '/') {
+      if(!user){
+        window.location.href = '/';
+        
+      }
+      else{
+        return (
+            <h3>Bienvenido: {user} </h3>
+    
+        )
+    
+      }
+
+    }
 
     }
 
--- src/Components/LoginView/LoginView.jsx
+++ src/Components/LoginView/LoginView.jsx
@@ -1,9 +1,48 @@
 import React from "react";
-
+import axios from "axios";
 import { Link } from "react-router-dom";
-
+import {useHistory} from "react-router-dom"
+import Cookies from 'js-cookie';
 
 function Login() {
+
+
+  const buscarUsuario = async ()=>{
+    var user = document.getElementById("usuario").value
+    var pass = document.getElementById("contra").value
+    console.log('https://localhost:7201/Usuario/Login/' + user +"/"+pass)
+    if (user != "" & pass != ""){
+      const respuesta = await axios.put('https://localhost:7201/Usuario/Login/' + user +"/"+pass)
+      console.log(respuesta.data.result.length)
+      if (respuesta.data.result.length > 0) { // Verifica si la respuesta es true
+        setTimeout(function() {
+          console.log("Espera de 3 segundos");
+        }, 8000);
+        document.getElementById("usuario").style.borderColor = 'green'
+      document.getElementById("contra").style.borderColor = 'green'
+
+
+
+
+      Cookies.set('Usuario', user, { expires: 1 / (24 * 60) });
+        window.location.href = '/MainMenu';
+        
+      } else {
+        console.log("ninguna coincidencia")
+        document.getElementById("usuario").style.borderColor = 'red'
+      document.getElementById("contra").style.borderColor = 'red'
+      document.getElementById("outmen").innerHTML = "El usuario no existe"
+      document.getElementById("outmen").hidden = false
+      }
+    }
+    else{
+      document.getElementById("usuario").style.borderColor = 'red'
+      document.getElementById("contra").style.borderColor = 'red'
+      document.getElementById("outmen").innerHTML = "Rellena todos los datos"
+      document.getElementById("outmen").hidden = false
+    }
+  
+  }
     return (
       <div className="Auth-form-container">
       <form className="Auth-form">
@@ -11,7 +50,7 @@ function Login() {
           <h3 className="Auth-form-title">Iniciar sesion</h3>
           <div className="form-group mt-3">
             <label>Username</label>
-            <input
+            <input id="usuario"
               type="user"
               className="form-control mt-1"
               placeholder="Username"
@@ -19,24 +58,25 @@ function Login() {
           </div>
           <div className="form-group mt-3">
             <label>Contraseña</label>
-            <input
+            <input id="contra"
               type="password"
               className="form-control mt-1"
               placeholder="Contraseña"
             />
+            
           </div>
+          <label id="outmen" className="outputred" hidden>El usuario no existe</label>
           <div className="d-grid gap-2 mt-3">
             
-            <Link to='MainMenu'>
-              <button type="submit" className="btn btn-primary">
+              <div onClick={()=> buscarUsuario()} className="btn btn-primary">
                 Ingresar 
-              </button>
-            </Link>
+              </div>
+            
           </div>
           <hr class="mx-n3"/>
           <div className="d-grid gap-2 mt-3">
             <Link to='/SignIn'>
-              <button type="submit" className="btn btn-secondary">
+              <button className="btn btn-secondary">
                 Registrarse 
               </button>
             </Link>
