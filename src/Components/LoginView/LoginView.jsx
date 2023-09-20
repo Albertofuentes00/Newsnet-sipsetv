45a1cb8fe5c061e5f4111977d40fbf8cd34b9fc0
Alejandro <toothyx@hotmail.com>
2023-09-20 12:08:55 -0500

    si

--- src/Components/LoginView/LoginView.jsx
+++ src/Components/LoginView/LoginView.jsx
@@ -21,7 +21,7 @@ function Login() {
         document.getElementById("usuario").style.borderColor = 'green'
       document.getElementById("contra").style.borderColor = 'green'
 
-//hola
+//holas
 
 
       Cookies.set('Usuario', user);
